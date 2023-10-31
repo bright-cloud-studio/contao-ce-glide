2d2d653ddee64fd820bb9908bc35ff89711e17cf
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-10-31 13:15:44 -0400

    Create tl_content.php

---
+++ src/Resources/contao/dca/tl_content.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Glide
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-ce-glide
+ */
+
+// Get our default 'tl_content' DCA
+$dc = &$GLOBALS['TL_DCA']['tl_content'];
+
+$GLOBALS['TL_DCA']['tl_content']['palettes']['glide'] = '{type_legend},type,headline;{text_legend},text,buttonLink;{image_legend},addImage;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},cssID;{invisible_legend:hide},invisible,start,stop';
+
+$arrFields = array(
+    'buttonLink'                => array(
+        'label'                => &$GLOBALS['TL_LANG']['tl_content']['buttonLink'],
+        'inputType'            => 'text',
+        'eval'                 => array('mandatory'=>false, 'maxlength'=>255, 'feEditable'=>true, 'feViewable'=>true, 'feGroup'=>'personal', 'tl_class'=>'w50'),
+        'sql'                  => "varchar(255) NOT NULL default ''"
+    )
+);
+
+$dc['fields'] = array_merge($dc['fields'], $arrFields);
