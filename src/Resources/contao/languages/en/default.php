978727cadebe71544b8ef28e4cd0d2f09ef80b0c
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-10-31 13:18:44 -0400

    Create default.php

---
+++ src/Resources/contao/languages/en/default.php
@@ -0,0 +1,14 @@
+<?php
+
+  /**
+   * @copyright  Bright Cliud Studio
+   * @author     Bright Cloud Studio
+   * @package    Contao CE Glide
+   * @license    LGPL-3.0+
+   * @see	       https://github.com/bright-cloud-studio/contao-ce-glide
+   */
+
+  $GLOBALS['TL_LANG']['CTE']['glide'][0] = 'GlideTest';
+  $GLOBALS['TL_LANG']['CTE']['glide'][1] = 'Testing this thing out';
+
+?>
