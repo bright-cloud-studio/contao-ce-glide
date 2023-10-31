15c35c248df637cfcc7bd35855fe6345606d90a0
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-10-31 13:14:16 -0400

    Create config.php

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Glide
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-ce-glide
+ */
+  
+   array_insert($GLOBALS['TL_CTE']['glide'], 10, array
+   (
+      'glide' => 'Bcs\GlideBundle\ContentGlide')
+   );
+
+?>
