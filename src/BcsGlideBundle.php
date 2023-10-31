04d54802de16445abdeca3c47fbc8a9f34767178
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-10-31 12:57:49 -0400

    Create BcsGlideBundle.php

---
+++ src/BcsGlideBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Glide
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-ce-glide
+ */
+
+namespace Bcs\GlideBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsGlideBundle extends Bundle
+{
+}
