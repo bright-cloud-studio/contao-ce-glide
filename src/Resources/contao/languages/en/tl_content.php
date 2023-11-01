355d8c5a0a1434e7488737c785a15ce74a2b0e5d
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-11-01 14:57:53 -0400

    Update tl_content.php

--- src/Resources/contao/languages/en/tl_content.php
+++ src/Resources/contao/languages/en/tl_content.php
@@ -13,6 +13,12 @@
 $GLOBALS['TL_LANG']['tl_content']['glide_legend']    = 'Glide Settings';
 
 /* Fields */
-$GLOBALS['TL_LANG']['tl_content']['glide_type']        = array('Glide Type', 'Select the \'type\' setting for Glide');
-$GLOBALS['TL_LANG']['tl_content']['starting_slide']        = array('Starting Slide', 'Enter the number for which slide to start at');
+$GLOBALS['TL_LANG']['tl_content']['glide_type']          = array('Glide Type', 'Select the \'type\' setting for Glide');
+$GLOBALS['TL_LANG']['tl_content']['starting_slide']      = array('Starting Slide', 'Enter the number for which slide to start at');
+
+$GLOBALS['TL_LANG']['tl_content']['slides_to_show']      = array('Slides to Show', 'Enter the number for how many slides to show');
+$GLOBALS['TL_LANG']['tl_content']['slide_padding']       = array('Slide Padding', 'Enter the number for the padding between slides');
+$GLOBALS['TL_LANG']['tl_content']['autoplay']            = array('Autoplay', 'Choose if slides should autoplay or not and at what speed');
+$GLOBALS['TL_LANG']['tl_content']['pause_on_hover']      = array('Pause on Hover', 'Choose if the slides should pause when being hovered');
+$GLOBALS['TL_LANG']['tl_content']['ani_duration']        = array('Animation Duration', 'Enter the number in MS for the speed the animation plays');
 
