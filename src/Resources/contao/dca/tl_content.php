e1e6585db3849808bb5f77c5c7ec3ff34010aec5
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-11-01 14:42:24 -0400

    Update tl_content.php

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -11,15 +11,22 @@
 // Get our default 'tl_content' DCA
 $dc = &$GLOBALS['TL_DCA']['tl_content'];
 
-$GLOBALS['TL_DCA']['tl_content']['palettes']['glide_start'] = '{type_legend},type,headline;{slider_legend},sliderDelay,sliderSpeed,sliderStartSlide,sliderContinuous;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},cssID;{invisible_legend:hide},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['glide_start'] = '{type_legend},type,headline;{glide_legend},glide_type,starting_slide;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},cssID;{invisible_legend:hide},invisible,start,stop';
 $GLOBALS['TL_DCA']['tl_content']['palettes']['glide_stop'] = '{type_legend},type;{template_legend:hide},customTpl;{protected_legend:hide},protected;{invisible_legend:hide},invisible,start,stop';
     
 $arrFields = array(
-    'buttonLink'                => array(
-        'label'                => &$GLOBALS['TL_LANG']['tl_content']['buttonLink'],
-        'inputType'            => 'text',
-        'eval'                 => array('mandatory'=>false, 'maxlength'=>255, 'feEditable'=>true, 'feViewable'=>true, 'feGroup'=>'personal', 'tl_class'=>'w50'),
-        'sql'                  => "varchar(255) NOT NULL default ''"
+    'glide_type'                => array(
+        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['glide_type'],
+        'inputType'                => 'select',
+		'options'                  => array('slider' => 'Slider', 'carousel' => 'Carousel'),
+		'eval'                     => array('mandatory'=>true, 'tl_class'=>'w50'),
+		'sql'                      => "varchar(32) NOT NULL default 'slider'"
+    ),
+    'starting_slide'            => array(
+        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['starting_slide'],
+        'inputType'                => 'text',
+		'eval'                     => array('tl_class'=>'w50'),
+		'sql'                      => "varchar(255) NOT NULL default ''"
     )
 );
 
