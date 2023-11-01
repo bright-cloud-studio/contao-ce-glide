4a7e1a845939dd5c79ce9b13e3d1d8647db11693
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-11-01 14:16:24 -0400

    Update ContentGlideStart.php

--- src/Resources/contao/elements/ContentGlideStart.php
+++ src/Resources/contao/elements/ContentGlideStart.php
@@ -25,6 +25,17 @@ class ContentGlideStart extends \ContentText
 	 */
 	public function compile()
 	{
-		parent::compile();
+		$request = System::getContainer()->get('request_stack')->getCurrentRequest();
+
+		if ($request && System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest($request))
+		{
+			$this->strTemplate = 'be_wildcard';
+
+			$this->Template = new BackendTemplate($this->strTemplate);
+			$this->Template->title = $this->headline;
+		}
+
+		// Slider configuration
+		$this->Template->config = $this->sliderDelay . ',' . $this->sliderSpeed . ',' . $this->sliderStartSlide . ',' . $this->sliderContinuous;
 	}
 }
