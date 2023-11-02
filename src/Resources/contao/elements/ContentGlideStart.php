2dc6150162fca37acda0a1fe4d634b2092518832
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-11-02 10:18:01 -0400

    Update ContentGlideStart.php

--- src/Resources/contao/elements/ContentGlideStart.php
+++ src/Resources/contao/elements/ContentGlideStart.php
@@ -12,17 +12,10 @@ namespace Bcs\GlideBundle;
 
 class ContentGlideStart extends \ContentText
 {
-
-	/**
-	 * Template
-	 * @var string
-	 */
+	/* Template @var string */
 	protected $strTemplate = 'ce_glide_start';
 
-
-	/**
-	 * Generate the content element
-	 */
+	/* Generate the content element */
 	public function compile()
 	{
 		$request = \System::getContainer()->get('request_stack')->getCurrentRequest();
@@ -30,12 +23,11 @@ class ContentGlideStart extends \ContentText
 		if ($request && \System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest($request))
 		{
 			$this->strTemplate = 'be_wildcard';
-
 			$this->Template = new \BackendTemplate($this->strTemplate);
 			$this->Template->title = $this->headline;
 		}
 
 		// Slider configuration
-		$this->Template->config = $this->sliderDelay . ',' . $this->sliderSpeed . ',' . $this->sliderStartSlide . ',' . $this->sliderContinuous;
+		$this->Template->config = $this->glide_type . ',' . $this->starting_slide . ',' . $this->slides_to_show . ',' . $this->slide_padding . ',' . $this->autoplay . ',' . $this->pause_on_hover . ',' . $this->ani_duration . ',' . $this->keyboard . ',' . $this->peek;
 	}
 }
