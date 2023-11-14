9ab154cfe7914b4c1cd4a627c1f2d14b60aeceb1
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-11-14 12:48:55 -0500

    Update default.php

--- src/Resources/contao/languages/en/default.php
+++ src/Resources/contao/languages/en/default.php
@@ -9,10 +9,14 @@
    */
 
     $GLOBALS['TL_LANG']['CTE']['glide'] = 'Glide Slider';
+    
     $GLOBALS['TL_LANG']['CTE']['glide_start'][0] = 'Glide Start';
     $GLOBALS['TL_LANG']['CTE']['glide_start'][1] = 'Starting wrapper for the Glide slider';
 
     $GLOBALS['TL_LANG']['CTE']['glide_stop'][0] = 'Glide Stop';
     $GLOBALS['TL_LANG']['CTE']['glide_stop'][1] = 'End of wrapper for the Glide slider';
+
+    $GLOBALS['TL_LANG']['CTE']['glide_gallery'][0] = 'Glide Gallery';
+    $GLOBALS['TL_LANG']['CTE']['glide_gallery'][1] = 'Select images and display them as a gallery using Glide';
 
 ?>
