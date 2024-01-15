2f91c98f502cc326e944c0a14453917dd7bce6a9
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-01-15 13:36:44 -0500

    Update tl_files.php

--- src/Resources/contao/dca/tl_files.php
+++ src/Resources/contao/dca/tl_files.php
@@ -25,32 +25,3 @@ $arrFields = array(
 
 $dc['fields'] = array_merge($dc['fields'], $arrFields);
 
-
-public function adjustPalettes(DataContainer $dc)
-{
-    if (!$dc->id)
-    {
-        return;
-    }
-
-    $projectDir = System::getContainer()->getParameter('kernel.project_dir');
-    $blnIsFolder = is_dir($projectDir . '/' . $dc->id);
-
-    // Remove the metadata when editing folders
-    if ($blnIsFolder)
-    {
-        PaletteManipulator::create()
-            ->removeField('meta')
-            ->applyToPalette('default', $dc->table)
-        ;
-    }
-
-    // Only show the important part fields for images
-    if ($blnIsFolder || !in_array(strtolower(substr($dc->id, strrpos($dc->id, '.') + 1)), System::getContainer()->getParameter('contao.image.valid_extensions')))
-    {
-        PaletteManipulator::create()
-            ->removeField(array('importantPartX', 'importantPartY', 'importantPartWidth', 'importantPartHeight', 'gallery_number'))
-            ->applyToPalette('default', $dc->table)
-        ;
-    }
-}
