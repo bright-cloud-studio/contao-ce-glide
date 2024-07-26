a0c46ab885636cacc6c17b15cbd78bca12e206c0
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-26 13:42:37 -0400

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -8,12 +8,12 @@
  * @see	       https://github.com/bright-cloud-studio/contao-ce-glide
  */
   
-   array_insert($GLOBALS['TL_CTE']['glide'], -1, array
-    (
-        'glide_start' => 'Bcs\GlideBundle\ContentGlideStart',
-        'glide_stop' => 'Bcs\GlideBundle\ContentGlideStop',
-        'glide_gallery' => 'Bcs\GlideBundle\ContentGlideGallery'
-    ));
+   //array_insert($GLOBALS['TL_CTE']['glide'], -1, array
+    //(
+      //  'glide_start' => 'Bcs\GlideBundle\ContentGlideStart',
+       // 'glide_stop' => 'Bcs\GlideBundle\ContentGlideStop',
+      // 'glide_gallery' => 'Bcs\GlideBundle\ContentGlideGallery'
+   // ));
 
 $GLOBALS['TL_CTE']['glide']['glide_start'] = 'Bcs\GlideBundle\ContentGlideStart';
 $GLOBALS['TL_CTE']['glide']['glide_stop'] = 'Bcs\GlideBundle\ContentGlideStop';
