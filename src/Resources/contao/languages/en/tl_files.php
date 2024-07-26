88eb2a39ecbe9845a9439617793714abf3bbdcc7
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-26 12:13:39 -0400

    Create tl_files.php

---
+++ src/Resources/contao/languages/en/tl_files.php
@@ -0,0 +1,7 @@
+<?php
+
+/* Frasch Specific */
+$GLOBALS['TL_LANG']['tl_files']['glide_number']        = array('Glide Number', 'Enter the number that displays in the lower right of a gallery thumbnail');
+$GLOBALS['TL_LANG']['tl_files']['glide_name']          = array('Glide Name', 'Enter the name that displays below a gallery thumbnail');
+$GLOBALS['TL_LANG']['tl_files']['glide_new']           = array('Glide New Circle', 'Choose if a thumbnail should be marked as \'NEW\' or not');
+$GLOBALS['TL_LANG']['tl_files']['glide_featured']      = array('Glide Featured', 'Choose if a thumbnail should be marked as \'Featured\' or not');
