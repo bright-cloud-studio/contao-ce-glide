e83c224397805d53bbc81fafbca72b2179c12259
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-29 08:13:20 -0400

    Update ContentGlideStop.php

--- src/Resources/contao/elements/ContentGlideStop.php
+++ src/Resources/contao/elements/ContentGlideStop.php
@@ -10,7 +10,9 @@
 
 namespace Bcs\GlideBundle;
 
+use Contao\BackendTemplate;
 use Contao\ContentText;
+use Contao\System;
 
 class ContentGlideStop extends ContentText
 {
@@ -21,12 +23,12 @@ class ContentGlideStop extends ContentText
 	/* Generate the content element */
 	public function compile()
 	{
-		$request = \System::getContainer()->get('request_stack')->getCurrentRequest();
+		$request = System::getContainer()->get('request_stack')->getCurrentRequest();
 
-		if ($request && \System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest($request))
+		if ($request && System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest($request))
 		{
 			$this->strTemplate = 'be_wildcard';
-			$this->Template = new \BackendTemplate($this->strTemplate);
+			$this->Template = new BackendTemplate($this->strTemplate);
 		}
 
 		// Previous and next labels
