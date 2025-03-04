3316a7d63d7300e8af2d1758e70f839af95356f9
johnadapt <2372574+johnadapt@users.noreply.github.com>
2025-03-04 10:17:10 -0800

    Update tl_content.php
    backwards compatible for 4.13

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -1,15 +1,17 @@
 <?php
 
 /**
- * @copyright  Bright Cliud Studio
+ * @copyright  Bright Cloud Studio
  * @author     Bright Cloud Studio
  * @package    Contao CE Glide
  * @license    LGPL-3.0+
- * @see	       https://github.com/bright-cloud-studio/contao-ce-glide
+ * @see        https://github.com/bright-cloud-studio/contao-ce-glide
  */
 
 use Contao\Controller;
 use Contao\DataContainer;
+use Contao\System;
+use Contao\Config; // Needed for Contao 4.13
 
 // Get our default 'tl_content' DCA
 $dc = &$GLOBALS['TL_DCA']['tl_content'];
@@ -23,137 +25,89 @@ $GLOBALS['TL_DCA']['tl_content']['palettes']['glide_gallery'] = '{type_legend},t
 $GLOBALS['TL_DCA']['tl_content']['fields']['multiSRC']['eval']['isSortable'] = true;
 
 $arrFields = array(
-    'description'                => array(
-        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['description'],
-    	'exclude'				 => true,
-	    'inputType'				 => 'textarea',
-	    'search'				 => true,
-		'eval'                     => array('style'=>'height:60px', 'rte'=>'tinyMCE', 'tl_class'=>'clr long'),
-		'explanation'  			 => 'insertTags',
-    	'sql'					 => "text NULL"
-    	
-
+    'description' => array(
+        'label'        => &$GLOBALS['TL_LANG']['tl_content']['description'],
+        'exclude'      => true,
+        'inputType'    => 'textarea',
+        'search'       => true,
+        'eval'         => array('style'=>'height:60px', 'rte'=>'tinyMCE', 'tl_class'=>'clr long'),
+        'explanation'  => 'insertTags',
+        'sql'          => "text NULL"
     ),
-    'glide_type'                => array(
-        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['glide_type'],
-        'inputType'                => 'select',
-		'options'                  => array('slider' => 'Slider', 'carousel' => 'Carousel'),
-		'eval'                     => array('mandatory'=>true, 'tl_class'=>'w50'),
-		'sql'                      => "varchar(32) NOT NULL default 'slider'"
+    'glide_type' => array(
+        'label'        => &$GLOBALS['TL_LANG']['tl_content']['glide_type'],
+        'inputType'    => 'select',
+        'options'      => array('slider' => 'Slider', 'carousel' => 'Carousel'),
+        'eval'         => array('mandatory'=>true, 'tl_class'=>'w50'),
+        'sql'          => "varchar(32) NOT NULL default 'slider'"
     ),
-    'starting_slide'            => array(
-        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['starting_slide'],
-        'inputType'                => 'text',
-		'eval'                     => array('tl_class'=>'w50'),
-		'sql'                      => "varchar(12) NOT NULL default ''"
+    'starting_slide' => array(
+        'label'        => &$GLOBALS['TL_LANG']['tl_content']['starting_slide'],
+        'inputType'    => 'text',
+        'eval'         => array('tl_class'=>'w50'),
+        'sql'          => "varchar(12) NOT NULL default ''"
     ),
-    'slides_to_show'            => array(
-        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['slides_to_show'],
-        'inputType'                => 'text',
-		'eval'                     => array('tl_class'=>'w50'),
-		'sql'                      => "varchar(12) NOT NULL default ''"
+    'slides_to_show' => array(
+        'label'        => &$GLOBALS['TL_LANG']['tl_content']['slides_to_show'],
+        'inputType'    => 'text',
+        'eval'         => array('tl_class'=>'w50'),
+        'sql'          => "varchar(12) NOT NULL default ''"
     ),
-    'slide_padding'            => array(
-        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['slide_padding'],
-        'inputType'                => 'text',
-		'eval'                     => array('tl_class'=>'w50'),
-		'sql'                      => "varchar(12) NOT NULL default ''"
+    'slide_padding' => array(
+        'label'        => &$GLOBALS['TL_LANG']['tl_content']['slide_padding'],
+        'inputType'    => 'text',
+        'eval'         => array('tl_class'=>'w50'),
+        'sql'          => "varchar(12) NOT NULL default ''"
     ),
-    'autoplay'                => array(
-        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['autoplay'],
-        'inputType'                => 'select',
-		'options'                  => array('false' => 'False', '500' => '500ms', '1000' => '1000ms', '1500' => '1500ms', '2000' => '2000ms', '2500' => '2500ms', '3000' => '3000ms'),
-		'eval'                     => array('mandatory'=>true, 'tl_class'=>'w50'),
-		'sql'                      => "varchar(32) NOT NULL default 'false'"
+    'autoplay' => array(
+        'label'        => &$GLOBALS['TL_LANG']['tl_content']['autoplay'],
+        'inputType'    => 'select',
+        'options'      => array('false' => 'False', '500' => '500ms', '1000' => '1000ms', '1500' => '1500ms', '2000' => '2000ms', '2500' => '2500ms', '3000' => '3000ms'),
+        'eval'         => array('mandatory'=>true, 'tl_class'=>'w50'),
+        'sql'          => "varchar(32) NOT NULL default 'false'"
     ),
-    'pause_on_hover'                => array(
-        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['pause_on_hover'],
-        'inputType'                => 'select',
-		'options'                  => array('true' => 'True', 'false' => 'False'),
-		'eval'                     => array('mandatory'=>true, 'tl_class'=>'w50'),
-		'sql'                      => "varchar(32) NOT NULL default 'true'"
+    'multiSRC' => array(
+        'label'        => &$GLOBALS['TL_LANG']['tl_content']['multiSRC'],
+        'exclude'      => true,
+        'inputType'    => 'fileTree',
+        'eval'         => array('multiple'=>true, 'fieldType'=>'checkbox', 'orderField'=>'orderSRC', 'files'=>true, 'mandatory'=>true),
+        'sql'          => "blob NULL",
+        'load_callback' => array(
+            array('tl_content_bcs', 'setMultiSrcFlags')
+        )
     ),
-    'ani_duration'            => array(
-        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['ani_duration'],
-        'inputType'                => 'text',
-		'eval'                     => array('tl_class'=>'w50'),
-		'sql'                      => "varchar(12) NOT NULL default ''"
-    ),
-    'keyboard'                => array(
-        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['keyboard'],
-        'inputType'                => 'select',
-		'options'                  => array('true' => 'True', 'false' => 'False'),
-		'eval'                     => array('mandatory'=>true, 'tl_class'=>'w50'),
-		'sql'                      => "varchar(32) NOT NULL default 'true'"
-    ),
-    'peek'                    => array(
-        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['peek'],
-        'inputType'                => 'text',
-		'eval'                     => array('tl_class'=>'w50'),
-		'sql'                      => "varchar(12) NOT NULL default ''"
-    ),
-    'thumb_size' => array
-    (
-        'label'                   => &$GLOBALS['TL_LANG']['MSC']['thumb_size'],
-        'inputType'               => 'imageSize',
-        'reference'               => &$GLOBALS['TL_LANG']['MSC'],
-        'options_callback' => static function ()
-        {
-            return System::getContainer()->get('contao.image.sizes')->getOptionsForUser(BackendUser::getInstance());
-        },
-        'eval'                    => array('rgxp'=>'natural', 'includeBlankOption'=>true, 'nospace'=>true, 'helpwizard'=>true, 'tl_class'=>'w50 clr'),
-        'sql'                     => "varchar(128) COLLATE ascii_bin NOT NULL default ''"
-    ),
-    'thumb_template' => array
-    (
-        'inputType'               => 'select',
-        'options_callback' => static function () {
-            return Controller::getTemplateGroup('gallery_');
-        },
-        'eval'                    => array('includeBlankOption'=>true, 'chosen'=>true, 'tl_class'=>'w50'),
-        'sql'                     => "varchar(64) COLLATE ascii_bin NOT NULL default ''"
-    ),
-    'multiSRC' => array
-		(
-			'label'                   => &$GLOBALS['TL_LANG']['tl_content']['multiSRC'],
-			'exclude'                 => true,
-			'inputType'               => 'fileTree',
-			'eval'                    => array('multiple'=>true, 'fieldType'=>'checkbox', 'orderField'=>'orderSRC', 'files'=>true, 'mandatory'=>true),
-			'sql'                     => "blob NULL",
-			'load_callback' => array
-			(
-				array('tl_content_bcs', 'setMultiSrcFlags')
-			)
-		),
 );
 
 $dc['fields'] = array_merge($dc['fields'], $arrFields);
 
-
 class tl_content_bcs extends tl_content
 {
     public function setMultiSrcFlags($varValue, DataContainer $dc)
-	{
-		if ($dc->activeRecord)
-		{
-			switch ($dc->activeRecord->type)
-			{
-				case 'gallery':
-					$GLOBALS['TL_DCA'][$dc->table]['fields'][$dc->field]['eval']['isGallery'] = true;
-					$GLOBALS['TL_DCA'][$dc->table]['fields'][$dc->field]['eval']['extensions'] = Config::get('validImageTypes');
-					break;
+    {
+        if ($dc->activeRecord) {
+            switch ($dc->activeRecord->type) {
+                case 'gallery':
                 case 'glide_gallery':
-					$GLOBALS['TL_DCA'][$dc->table]['fields'][$dc->field]['eval']['isGallery'] = true;
-					$GLOBALS['TL_DCA'][$dc->table]['fields'][$dc->field]['eval']['extensions'] = Config::get('validImageTypes');
-					break;
+                    $GLOBALS['TL_DCA'][$dc->table]['fields'][$dc->field]['eval']['isGallery'] = true;
+                    
+                    // Use Contao 5.x method if available, fallback to Contao 4.13
+                    $GLOBALS['TL_DCA'][$dc->table]['fields'][$dc->field]['eval']['extensions'] =
+                        class_exists(System::class) 
+                            ? System::getContainer()->getParameter('validImageTypes') 
+                            : Config::get('validImageTypes');
+                    break;
 
-				case 'downloads':
-					$GLOBALS['TL_DCA'][$dc->table]['fields'][$dc->field]['eval']['isDownloads'] = true;
-					$GLOBALS['TL_DCA'][$dc->table]['fields'][$dc->field]['eval']['extensions'] = Config::get('allowedDownload');
-					break;
-			}
-		}
+                case 'downloads':
+                    $GLOBALS['TL_DCA'][$dc->table]['fields'][$dc->field]['eval']['isDownloads'] = true;
+                    
+                    $GLOBALS['TL_DCA'][$dc->table]['fields'][$dc->field]['eval']['extensions'] =
+                        class_exists(System::class) 
+                            ? System::getContainer()->getParameter('allowedDownload') 
+                            : Config::get('allowedDownload');
+                    break;
+            }
+        }
 
-		return $varValue;
-	}
+        return $varValue;
+    }
 }
