e839c5e51776bcde8541b952dc88252942bebbb9
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-06-04 12:02:24 -0400

    Add 'Glide In Stock' language entry

--- src/Resources/contao/languages/en/tl_content.php
+++ src/Resources/contao/languages/en/tl_content.php
@@ -29,6 +29,7 @@ $GLOBALS['TL_LANG']['tl_content']['thumb_size']          = array('Thumbnail Size
 $GLOBALS['TL_LANG']['tl_content']['glide_number']        = array('Glide Number', 'Enter the number that displays in the lower right of a gallery thumbnail');
 $GLOBALS['TL_LANG']['tl_content']['glide_name']          = array('Glide Name', 'Enter the name that displays below a gallery thumbnail');
 $GLOBALS['TL_LANG']['tl_content']['glide_new']           = array('Glide New Circle', 'Choose if a thumbnail should be marked as \'NEW\' or not');
+$GLOBALS['TL_LANG']['tl_content']['glide_in_stock']      = array('Glide In Stock', 'Choose if a thumbnail should be marked as \'In Stock\' or not');
 $GLOBALS['TL_LANG']['tl_content']['glide_featured']      = array('Glide Featured', 'Choose if a thumbnail should be marked as \'Featured\' or not');
 /* Soundseal Specific */
 $GLOBALS['TL_LANG']['tl_content']['glide_example_img']   = array('Glide Example Image', 'Select an image that you wish to override the main image shown when the thumbnail is selected. This is the \'Example\' of the thumbnail image');
