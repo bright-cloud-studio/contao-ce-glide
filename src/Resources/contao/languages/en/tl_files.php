29721865ca0a78f02016e8443c9c271669a5fd12
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-06-04 11:38:42 -0400

    Update language settings for glide features

--- src/Resources/contao/languages/en/tl_files.php
+++ src/Resources/contao/languages/en/tl_files.php
@@ -1,9 +1,21 @@
 <?php
 
-/* Frasch Specific */
+/* Default Files fields */
+$GLOBALS['TL_LANG']['tl_files']['file_settings']   = 'File - Settings';
+
+/* General Glide Settings */
+$GLOBALS['TL_LANG']['tl_files']['glide_settings']   = 'Glide - Settings';
 $GLOBALS['TL_LANG']['tl_files']['glide_number']        = array('Glide Number', 'Enter the number that displays in the lower right of a gallery thumbnail');
 $GLOBALS['TL_LANG']['tl_files']['glide_name']          = array('Glide Name', 'Enter the name that displays below a gallery thumbnail');
+
+/* Settings for 'New' */
+$GLOBALS['TL_LANG']['tl_files']['glide_new_settings']   = 'Glide - New - Settings';
 $GLOBALS['TL_LANG']['tl_files']['glide_new']           = array('Glide New Circle', 'Choose if a thumbnail should be marked as \'NEW\' or not');
+
+/* Settings for 'Featured' */
+$GLOBALS['TL_LANG']['tl_files']['glide_featured_settings']   = 'Glide - Featured - Settings';
 $GLOBALS['TL_LANG']['tl_files']['glide_featured']      = array('Glide Featured', 'Choose if a thumbnail should be marked as \'Featured\' or not');
-/* Soundseal Specific */
+
+/* Settings for 'Example' */
+$GLOBALS['TL_LANG']['tl_files']['glide_example_settings']   = 'Glide - Example - Settings';
 $GLOBALS['TL_LANG']['tl_files']['glide_example_img']      = array('Glide Example Image', 'Select an image that you wish to override the main image shown when the thumbnail is selected. This is the \'Example\' of the thumbnail image');
