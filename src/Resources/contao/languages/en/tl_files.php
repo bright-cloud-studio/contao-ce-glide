8680a25d5132b364056bcc2d1e3e0df4c58a835d
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-06-04 11:46:38 -0400

    Update tl_files.php

--- src/Resources/contao/languages/en/tl_files.php
+++ src/Resources/contao/languages/en/tl_files.php
@@ -1,21 +1,21 @@
 <?php
 
 /* Default Files fields */
-$GLOBALS['TL_LANG']['tl_files']['file_settings']   = 'File - Settings';
+$GLOBALS['TL_LANG']['tl_files']['file_settings_legend']   = 'File - Settings';
 
 /* General Glide Settings */
-$GLOBALS['TL_LANG']['tl_files']['glide_settings']   = 'Glide - Settings';
+$GLOBALS['TL_LANG']['tl_files']['glide_settings_legend']   = 'Glide - Settings';
 $GLOBALS['TL_LANG']['tl_files']['glide_number']        = array('Glide Number', 'Enter the number that displays in the lower right of a gallery thumbnail');
 $GLOBALS['TL_LANG']['tl_files']['glide_name']          = array('Glide Name', 'Enter the name that displays below a gallery thumbnail');
 
 /* Settings for 'New' */
-$GLOBALS['TL_LANG']['tl_files']['glide_new_settings']   = 'Glide - New - Settings';
+$GLOBALS['TL_LANG']['tl_files']['glide_new_settings_legend']   = 'Glide - New - Settings';
 $GLOBALS['TL_LANG']['tl_files']['glide_new']           = array('Glide New Circle', 'Choose if a thumbnail should be marked as \'NEW\' or not');
 
 /* Settings for 'Featured' */
-$GLOBALS['TL_LANG']['tl_files']['glide_featured_settings']   = 'Glide - Featured - Settings';
+$GLOBALS['TL_LANG']['tl_files']['glide_featured_settings_legend']   = 'Glide - Featured - Settings';
 $GLOBALS['TL_LANG']['tl_files']['glide_featured']      = array('Glide Featured', 'Choose if a thumbnail should be marked as \'Featured\' or not');
 
 /* Settings for 'Example' */
-$GLOBALS['TL_LANG']['tl_files']['glide_example_settings']   = 'Glide - Example - Settings';
+$GLOBALS['TL_LANG']['tl_files']['glide_example_settings_legend']   = 'Glide - Example - Settings';
 $GLOBALS['TL_LANG']['tl_files']['glide_example_img']      = array('Glide Example Image', 'Select an image that you wish to override the main image shown when the thumbnail is selected. This is the \'Example\' of the thumbnail image');
